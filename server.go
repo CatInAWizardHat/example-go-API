f1bd7d6ebf0b49e6a0cad9184e04d0279ab8943e
CatInAWizardHat <BeigeModel3@proton.me>
2025-05-07 15:13:44 -0700

    Added environment variable usage

--- server.go
+++ server.go
@@ -1,18 +1,30 @@
 package main
 
 import (	
+	"fmt"
+	"os"
 	"log"
-	"github.com/gin-gonic/gin"	
 	"example-message-api/services"
+
+	"github.com/gin-gonic/gin"	
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error, failed to load .env: %s", err)
+	}
+	host := os.Getenv("HOST_IP")
+	port := os.Getenv("HOST_PORT")
+	hostname := fmt.Sprintf("%s:%s", host, port)
+
 	messages := &services.Message{}
 	  r := gin.Default()
 	  r.GET("/messages", messages.GetMessages)
 	  r.GET("/messages/:id", messages.GetMessage)
 
-	  err := r.Run("localhost:8080")
+	  err = r.Run(hostname)
 	  if err != nil {
 	    log.Fatalf("Error, failed to start server: %s", err)
 	  }
