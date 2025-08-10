213f6a2e3605ae8e737748bd967f036bc29294bd
CatInAWizardHat <125680751+CatInAWizardHat@users.noreply.github.com>
2025-08-09 22:35:41 -0700

    test: defined TestHandler_Unit_GetMessage_Found

--- internal/message/handler_test.go
+++ internal/message/handler_test.go
@@ -3,6 +3,7 @@ package message
 import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	assert "github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -43,12 +44,38 @@ func (m *MockMessageStore) DeleteMessage(id string) error {
 }
 
 func TestHandler_Unit_GetMessage_Found(t *testing.T) {
+	id := uuid.New()
+	message := Message{
+		id,
+		"testuser",
+		"testtesttest",
+	}
 
+	mock := &MockMessageStore{
+		GetMessageResult: message,
+		GetMessageError:  nil,
+	}
+
+	handler := NewMessageHandler(mock)
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+
+	c.Params = gin.Params{{Key: "id", Value: id.String()}}
+	handler.GetMessage(c)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.True(t, strings.HasPrefix(w.Header().Get("Content-Type"), "application/json"))
+
+	var resp Message
+	err := json.Unmarshal(w.Body.Bytes(), &resp)
+	assert.NoError(t, err)
+	assert.Equal(t, message.User, resp.User)
+	assert.Equal(t, message.Text, resp.Text)
+	assert.Equal(t, message.ID, resp.ID)
 }
 
 func TestHandler_Unit_GetMessage_NotFound(t *testing.T) {
 	mock := &MockMessageStore{
-		GetMessageError:  ErrMessageNotFound,
+		GetMessageError: ErrMessageNotFound,
 	}
 
 	handler := NewMessageHandler(mock)
